68700b71f741769c7a1d4eb5d37b58e7f1b60ec9
Martin V <martinvillarreal90@gmail.com>
2020-10-29 03:43:57 +0100

    Test
    q

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
+		fetch('')
 			.then(response => response.json())
 			.then(users => this.setState({ robots: users }));
 	}
