786f23fb276747aabe6a2b1f0333b50347e7ed90
Martin V <martinvillarreal90@gmail.com>
2020-10-30 11:40:10 +0100

    Fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		fetch('')
+		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
 			.then(users => this.setState({ robots: users }));
 	}
